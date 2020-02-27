7827951e59ec482b43ca8eadf9ad19fe46100b0a
Vascord <duarte-vasco@outlook.com>
2020-02-27 11:08:35 +0000

    Added new code about types

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sbyte_numero = -5;
+            int int_numero = -10;
+            short short_numero = -20;
+            long long_numero = -30L;
+            byte byte_numero = 5;
+            uint uint_numero = 10U;
+            ushort ushort_numero = 20;
+            ulong ulong_numero = 30UL;
+            char character = '\u0052';
+            float float_numero = 3.45f;
+            double double_numero = 13.45;
+            decimal decimal_numero = 23.45m;
+            Console.WriteLine(sbyte_numero);
+            Console.WriteLine(int_numero);
+            Console.WriteLine(short_numero);
+            Console.WriteLine(long_numero);
+            Console.WriteLine(byte_numero);
+            Console.WriteLine(uint_numero);
+            Console.WriteLine(ushort_numero);
+            Console.WriteLine(ulong_numero);
+            Console.WriteLine(character);
+            Console.WriteLine(float_numero);
+            Console.WriteLine(double_numero);
+            Console.WriteLine(decimal_numero);
+        }
+    }
+}
