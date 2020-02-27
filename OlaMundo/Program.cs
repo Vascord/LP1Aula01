1a65599af0036f0cc1750b74a57d0ecdf5b9c87c
Vascord <duarte-vasco@outlook.com>
2020-02-27 09:42:00 +0000

    Added some new text

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -7,6 +7,8 @@ namespace OlaMundo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Change the world, my final message.");
+            Console.WriteLine("Goodbye.");
         }
     }
 }
