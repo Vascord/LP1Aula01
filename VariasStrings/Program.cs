26d9b273d8003d6d9bc47b4e073a39aef0661713
Vascord <duarte-vasco@outlook.com>
2020-02-27 11:40:00 +0000

    New code with strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string_1 = "Normal_string";
+            string string_2 = "String with spaces";
+            string string_3 = "\'String with unicode \u006E \u0069 \u0063 \u0065\'";
+            string string_4 = @"String verbatim º*'34»«";
+            Console.WriteLine(string_1);
+            Console.WriteLine(string_2);
+            Console.WriteLine(string_3);
+            Console.WriteLine(string_4);
+        }
+    }
+}
