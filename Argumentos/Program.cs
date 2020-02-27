3632f669320b0ff742b813f33b8c1796c91b14ba
Vascord <duarte-vasco@outlook.com>
2020-02-27 10:27:48 +0000

    Added commentaries

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Argumentos
 {
+    /// <summary>
+    /// Main class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// static void main with print for
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            for(int i = 0; i < args.Length; i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 Console.WriteLine(args[i]);
             }
